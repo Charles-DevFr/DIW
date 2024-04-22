01c572fd2b1f25a0401f2b152e72ec4c269f6d8e
Charles-DevFr <Charlesleul@gmail.com>
2024-04-22 15:42:52 +0200

    Commit

---
+++ Projet The District/TheDistrict_Contact.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formulaire = document.getElementById("formulaire");
+
+    formulaire.addEventListener('submit', function (e) {
+    {
+
+        e.preventDefault();
+
+    filtrealpha = new RegExp(/^[A-Za-z]+$/)
+    filtrenum = new RegExp(/^[0-9]+$/)
+    filtremail = new RegExp(/^[a-z0-9.-]+@[a-z0-9.-]+.[a-z0-9]+$/)
+    filtredate = new RegExp(/^[0-9]+-[0-9]+-[0-9]+$/)
+    var valide = true
+    var nom = document.getElementById("nom").value
+    var prenom = document.getElementById("prenom").value
+    var email = document.getElementById("email").value
+    var telephone = document.getElementById("telephone").value
+    var demande = document.getElementById("demande").value
+    var accepte = document.getElementById("accepte").value
+
+        resultat = filtrealpha.test(nom)
+           if (resultat == false)
+            {
+                valide = false
+                alert (" Entrez votre nom. ")
+            }
+        
+        resultat = filtrealpha.test(prenom)
+            if (resultat == false)
+            {
+                valide = false
+                alert (" Entrez votre prénom. ")
+            }
+        
+        resultat = filtremail.test(email)
+            if (resultat == false)
+            {
+                valide = false
+                alert (" Entrez un email valide. ")
+            }
+
+        resultat = filtrenum.test(telephone)
+            if (resultat == false)
+            {
+                valide = false
+                alert (" Entrez votre numéro de téléphone. ")
+            }
+
+        resultat = filtrealpha.test(demande)
+           if (resultat == false)
+            {
+                valide = false
+                alert (" Ecrivez votre question. ")
+            }
+        }
+    })
+})
+    
+    const choisi = document.getElementById("remove")
+    const choixElement = document.getElementById("sujet")
+    choixElement.addEventListener("change", () => 
+        {
+            choisi.remove()
+        }
+    )
