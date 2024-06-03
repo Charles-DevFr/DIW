d2ca4eefff1903f58b38c2354a3a587b24123e9b
Charles-DevFr <Charlesleul@gmail.com>
2024-06-03 15:47:28 +0200

    Fini

--- Projet The District/TheDistrict_Contact.js
+++ Projet The District/TheDistrict_Contact.js
@@ -1,70 +1,78 @@
+
 document.addEventListener('DOMContentLoaded', function () {
-  const formulaire = document.getElementById("formulaire");
+    const formulaire = document.getElementById("formulaire");
+    
+    
+    formulaire.addEventListener('submit', function (e) {
+        {
+            
+            e.preventDefault();
 
-  formulaire.addEventListener('submit', function (e) {
-  {
-
-      e.preventDefault();
-
-  filtrealpha = new RegExp(/^[A-Za-z]+$/)
-  filtrenum = new RegExp(/^(0|\+33 )[1-9]([-. ]?[0-9]{2} ){3}([-. ]?[0-9]{2})$/)
-  filtremail = new RegExp(/^[a-z0-9.-]+@[a-z0-9.-]+.[a-z0-9]+$/)
-  filtredate = new RegExp(/^[0-9]+-[0-9]+-[0-9]+$/)
-  var valide = true
-  var nom = document.getElementById("nom").value
-  var prenom = document.getElementById("prenom").value
-  var email = document.getElementById("email").value
-  var telephone = document.getElementById("telephone").value
-  var demande = document.getElementById("demande").value
-
-      resultat = filtrealpha.test(nom)
-         if (resultat == false)
-          {
-              valide = false
-              alert (" Entrez votre nom. ")
-          }
-      
-      resultat = filtrealpha.test(prenom)
-          if (resultat == false)
-          {
-              valide = false
-              alert (" Entrez votre prénom. ")
-          }
-      
-      resultat = filtremail.test(email)
-          if (resultat == false)
-          {
-              valide = false
-              alert (" Entrez un email valide. ")
-          }
-
-      resultat = filtrenum.test(telephone)
-          if (resultat == false)
-          {
-              valide = false
-              alert (" Entrez votre numéro de téléphone. ")
-          }
-
-      resultat = filtrealpha.test(demande)
-         if (resultat == false)
-          {
-              valide = false
-              alert (" Ecrivez votre question. ")
-          }
+            filtrealpha = new RegExp(/^[A-Za-z]+$/)
+            filtrenum = new RegExp(/^(0|\+33 )[1-9]([-. ]?[0-9]{2} ){3}([-. ]?[0-9]{2})$/)
+            filtremail = new RegExp(/^[a-z0-9.-]+@[a-z0-9.-]+.[a-z0-9]+$/)
+            filtredate = new RegExp(/^[0-9]+-[0-9]+-[0-9]+$/)
+            var nom = document.getElementById("nom").value
+            var prenom = document.getElementById("prenom").value
+            var email = document.getElementById("email").value
+            var telephone = document.getElementById("telephone").value
+            var demande = document.getElementById("demande").value
+            
+            resultat = filtrealpha.test(nom)
+              if (resultat == false)
+                {
+                    valide = false
+                    Swal.fire({
+                        title: "Nom invalide",
+                        text: "Veuillez entrer un nom valide",
+                        icon: "error"
+                      })
+                }
+                
+            resultat = filtrealpha.test(prenom)
+              if (resultat == false)
+                {
+                    valide = false
+                    Swal.fire({
+                        title: "Prénom invalide",
+                        text: "Veuillez entrer un prénom valide",
+                        icon: "error"
+                      })
+                }
+                    
+            resultat = filtremail.test(email)
+              if (resultat == false)
+                {
+                    valide = false
+                    Swal.fire({
+                        title: "email invalide",
+                        text: "Veuillez entrer un email valide",
+                        icon: "error"
+                      })
+                }
+                        
+            resultat = filtrenum.test(telephone)
+              if (resultat == false)
+                {
+                    valide = false
+                    Swal.fire({
+                        title: "Numéro de téléphone invalide",
+                        text: "Veuillez saisir un autre numéro de téléphone",
+                        icon: "error"
+                      })
+                }
+                        
+            resultat = filtrealpha.test(demande)
+              if (resultat == false)
+                {
+                    valide = false
+                    Swal.fire({
+                        title: "Demande invalide",
+                        text: "Veuillez entrer une demane valide",
+                        icon: "error"
+                      })
+                }
       }
   })
 })
 
-function createCustomAlert(txt) {
-    d = document;
-
-        if(d.getElementById("modalContainer")) return;
-
-    mObj = d.getElementByTagName("body")[0].appendChild(d.createElement("div"));
-    mObj.id = "modalContainer";
-    mObj.style.height = d.documentEmement.scrollHeight + "px";
-
-    alertObj = mObj.appendChild(d.createElement("div"));
-    alertObj.id = "alertBox";
-        if(d.all && !window.opera) alertObj.style.top = document.body.scrollTop + "
-}
--- Projet The District/TheDistrict_Contact.php
+++ Projet The District/TheDistrict_Contact.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The district Categorie</title>
+    <title>The district Contact</title>
     <link rel="shortcut icon" type="image/png" href="images_the_district/the_district_brand/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="TheDistrict_Contact.css">
 </head>
 <body id="body">
